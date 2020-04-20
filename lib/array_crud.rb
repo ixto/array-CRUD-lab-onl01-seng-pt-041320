d6fb4b057944a7db0f85f71b3c1db1d1939e2694
Joey <joey.alzamora@gmail.com>
2020-04-20 19:47:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,8 +3,8 @@ def create_an_empty_array
   
 end
 
-def create_an_array["earth", "wind", "water", "fire"]
-  
+def create_an_array
+  ["earth", "wind", "water", "fire"]
 end
 
 def add_element_to_end_of_array(array, element)
